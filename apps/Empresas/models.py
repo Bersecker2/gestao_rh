103e70d44eb492eed7a3badab4dba91ef0113f6a
Bersecker2 <rodrigo.eger.rock@gmail.com>
2019-11-10 11:03:22 -0300

    Criando a app Empresa

---
+++ apps/Empresas/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+#Representa entidade empresa do banco de dados
+class Empresa(models.Model):
+    Nome = models.CharField(max_length=70, help_text='Nome da Empresa')
+
+
+
+
+
+
+
